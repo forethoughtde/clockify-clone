c895029404b4d164d2c51505dda842edcc12a051
Karthikeyan Palaniswamy <karthikeyan.forethought@gmail.com>
2020-04-12 13:23:27 +0200

    Adapt the selector name of sidemenu, create a selector for close sidemenu button, and add a click event handler for the close sidemenu button.

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,6 +1,7 @@
 (function(){
   var $burgerButton = document.getElementById('clockify_menu_btn');
-  var $sideMenu = document.getElementById('clockify_sidebar');
+  var $sideMenu = document.getElementById('clockify_sidemenu');
+  var $sideMenuCloseBtn = document.getElementById('clockify_sidemenu_close_btn');
   var SIDEMENU_OPEN_CLASS = 'clockify_sidebar-open';
 
   function burgerButtonClickHandler(event){
@@ -11,6 +12,11 @@
     elementsMethod.removeClassFromElement($sideMenu, SIDEMENU_OPEN_CLASS);
   }
 
+  function sideMenuCloseBtnClockHandler(event){
+    $burgerButton.click();
+  }
+
   $burgerButton.addEventListener('click', burgerButtonClickHandler);
+  $sideMenuCloseBtn.addEventListener('click', sideMenuCloseBtnClockHandler)
 
 })();
